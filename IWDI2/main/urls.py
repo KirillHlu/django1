8a589eb8c6c0b0b0c7d0168a8246cfc9e5ef3d38
KirillHlu <ksmit3190@gmail.com>
2024-08-24 13:06:07 +0700

    first

--- IWDI2/main/urls.py
+++ IWDI2/main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.news_home, name='news-home'),  # Добавлено имя для главной страницы
-    path('about/', views.about, name='about'),    # Добавлено слэш в конце для согласованности
-    path('<int:pk>/', views.NewsDetailView.as_view(), name='news-detail')
+    path('<int:pk>/', views.NewsDetailView.as_view(), name='news-detail'),
+    path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
+    path('create_article/', views.create_article, name='create_article'),
+    path('accounts/profile/', views.news_home, name='news-home'),
 ]
